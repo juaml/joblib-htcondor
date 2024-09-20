7e0a486aff9e296f05f99f3d7a1b617a2ff9db83
Synchon Mandal <synchon@protonmail.com>
2024-09-20 16:52:35 +0200

    chore(ui): add type annotations and improve commentary in uilogging.py

--- joblib_htcondor/ui/uilogging.py
+++ joblib_htcondor/ui/uilogging.py
@@ -1,10 +1,31 @@
+"""The joblib htcondor UI logging."""
+
+# Authors: Federico Raimondo <f.raimondo@fz-juelich.de>
+# License: AGPL
+
 import logging
+
+
+__all__ = ["init_logging"]
 
 
 logger = logging.getLogger("joblib_htcondor.ui")
 
-def init_logging(level):
+
+def init_logging(level: int) -> None:
+    """Initialise logging for UI.
+
+    Parameters
+    ----------
+    level : int
+        The log level to set.
+
+    """
+    # Set logger level
     logger.setLevel(level)
+    # Create log file handler
     fh = logging.FileHandler("ui.log")
+    # Set handler level
     fh.setLevel(level)
+    # Add log file handler
     logger.addHandler(fh)
